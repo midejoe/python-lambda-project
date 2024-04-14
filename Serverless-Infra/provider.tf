b3ffee6766a4aed8a4c197f2b0c302159022b04a
Joseph Ariyo <midejoseph24@gmail.com>
2024-04-14 12:13:03 +0100

    modify codebuild file

--- Serverless-Infra/provider.tf
+++ Serverless-Infra/provider.tf
@@ -1,13 +1,15 @@
+#terraform provider
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.44.0"
     }
   }
 }
 
+#aws provider
 provider "aws" {
   profile = "vscode"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
